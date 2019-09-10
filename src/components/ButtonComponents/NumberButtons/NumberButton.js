1d91d9a76d44f4077fca4fb7319ba4b56df56e07
Fresco Fresco <fresco@Frescos-MacBook-Pro.local>
2019-09-10 17:09:52 -0400

    buttons created

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import Numbers from "./Numbers";
+
 
 const NumberButton = () => {
   return (
-    <>
+    <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      <Numbers />
+    </div>
   );
 };
+
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Specials from './Specials';
 
 const SpecialButton = () => {
   return (
-    <>
+    <div>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      <Specials />
+    </div>
   );
 };
+
+
+export default SpecialButton;
